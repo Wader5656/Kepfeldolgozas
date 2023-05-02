61336379b22720b0fea8d48683a0ec725447bf79
Wader5656 <tarilevente09@gmail.com>
2023-05-02 21:45:00 +0200

    update

--- suli.py
+++ suli.py
@@ -4,7 +4,9 @@ import glob
 import statistics as stat
 
 def main():
-    images = [cv2.imread(file) for file in glob.glob("D://programok//kepf//project//kep//kep*.jpg")]
+    images = [cv2.imread(file) for file in glob.glob("D://programok//kepf//beka//beka*.jpg")]
+    #images = [cv2.imread(file) for file in glob.glob("D://programok//kepf//fo//fo*.jpg")]
+    #images = [cv2.imread(file) for file in glob.glob("D://programok//kepf//kep//kep*.jpg")]
     img_res =  np.zeros((790, 1053, 3), dtype = "uint8")
     resized = []
     separated = []
@@ -30,24 +32,21 @@ def main():
                         tmpg.append(separated[l][k][i][j])
                     else:
                         tmpr.append(separated[l][k][i][j])
-            pix.append([tmpb,tmpg,tmpr])
+            pix.append([np.mean(tmpb),np.mean(tmpg),np.mean(tmpr)]) #44,7 sec kb
+            #pix.append([stat.mean(tmpb),stat.mean(tmpg),stat.mean(tmpr)]) #1:40 min kb, de pontosabb
+            #pix.append([np.median(tmpb),np.median(tmpg),np.median(tmpr)]) #29 sec kb
+            #pix.append([stat.median(tmpb),stat.median(tmpg),stat.median(tmpr)]) #23 sec kb, gyorsabb és elvileg pontosabb
             tmpb = []
             tmpg = []
             tmpr = []
 
-    res = []
-    tmp = []
-    for i in range(0,len(pix)):
-        for j in range(0,len(pix[0])):
-           tmp.append(stat.median(pix[i][j]))
-        res.append(tmp)
-        tmp = []
+    
 
     for i in range(0,790):
         for j in range(0,1053):
-            if len(res) > 0:
-                img_res[i][j] = (res[j][0],res[j][1],res[j][2]) 
-        res[0:1053] = []
+            if len(pix) > 0:
+                img_res[i][j] = (pix[j][0],pix[j][1],pix[j][2]) 
+        pix[0:1053] = []
 
     cv2.imshow("kép",img_res)
     cv2.waitKey(0)
